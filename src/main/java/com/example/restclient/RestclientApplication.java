e58ae5fa1cec816629b912d62c1129e61b887e3c
marszm <mariusz.szmer@gmail.com>
2019-10-02 18:15:16 +0200

    simple example with github

--- src/main/java/com/example/restclient/RestclientApplication.java
+++ src/main/java/com/example/restclient/RestclientApplication.java
@@ -1,16 +1,22 @@
 package com.example.restclient;
 
 import feign.Feign;
+import feign.gson.GsonDecoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class RestclientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestclientApplication.class, args);
+
+        GitHUB gitHUB = Feign.builder().decoder(new GsonDecoder()).target(GitHUB.class, "https://api.github.com");
+        List<Contributor> contributorList = gitHUB.contributors("OpenFeign","feign");
+        for(Contributor contributor: contributorList) {
+            System.out.println(contributor.login+""+contributor.contributions);
+        }
     }
-
-    GitHUB gitHUB = Feign.builder().decoder(new Gson).target(GitHUB.class, "https://api.github.com");
-
 }
